1b4190f677ba09109c39779ea0a8a069ae419e3a
MrRocketR <kuptovns@gmail.com>
2021-10-10 14:03:16 +0300

    Для бота

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -12,16 +12,18 @@ public class Win {
         }
         return result;
     }
+
     public static boolean monoVertical(int[][] board, int column) {
         boolean result = true;
-        for (int row = 0; row < board.length; row++) {
-            if (board[row][column] != 1) {
+        for (int[] ints : board) {
+            if (ints[column] != 1) {
                 result = false;
                 break;
             }
         }
         return result;
     }
+
     public static boolean check(int[][] board) {
         boolean rsl = false;
         for (int index = 0; index < board.length; index++) {
